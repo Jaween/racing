3f290650cf97983876e522912a45464857176cf2
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2016-01-23 13:17:42 +1030

    Add camera controller

---
+++ Assets/Scripts/General/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject cameraNode;
+    public float distanceFromNode;
+    public float maxPositionLag;
+    public float maxPositionLerpMultiplier;
+    public float maxRotationLag;
+    public float rotationLerpMultiplier;
+    public float minFov;
+
+    void Update()
+    {
+        Vector3 fromPosition = transform.position;
+        Vector3 toPosition = cameraNode.transform.position -
+            distanceFromNode * cameraNode.transform.forward;
+        transform.position = Vector3.Lerp(fromPosition, toPosition,
+            Time.deltaTime * maxPositionLerpMultiplier);
+
+        Quaternion fromRotation = transform.rotation;
+        Quaternion toRotation = cameraNode.transform.rotation;
+        transform.rotation = Quaternion.Slerp(fromRotation, toRotation,
+              Time.deltaTime * rotationLerpMultiplier);
+    }
+}
